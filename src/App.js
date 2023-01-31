a3693477b8498e1f0d25a7ca3977df8f2b06f97e
alirazasdk <alirazasdk10@gmail.com>
2023-01-31 12:34:53 +0500

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
+import About from './pages/About';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <><Home/></>
+    <>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </>
   );
 }
 
