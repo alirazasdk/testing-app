f675182456b5eb4d21948e638cb69ae3d545fcbe
alirazasdk <alirazasdk10@gmail.com>
2023-01-31 12:47:09 +0500

    Applied basic Helmet

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+
 
 function App() {
   return (
     <>
       <Router>
+        <Helmet>
+          <title>Learning React Helmet!</title>
+          <meta name='description' content='Beginner friendly page for learning React Helmet.' />
+        </Helmet>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react";
+import axios from "axios";
 const Home=()=>{
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/posts/1')
+    .then(((res)=>{
+      console.log('res',res.data)
+    }))
+  },[])
   return(<>
   <h1>This is Home</h1>
   </>);
