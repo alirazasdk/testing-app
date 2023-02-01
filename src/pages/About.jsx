0cf9d3d4f4d0f971de6b9bd21a226a8feb19f31e
alirazasdk <alirazasdk10@gmail.com>
2023-02-01 10:24:08 +0500

    Applied react helmet async inside about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,11 @@
+
+import { Helmet } from 'react-helmet-async';
 const About=()=>{
   return(<>
+  <Helmet>
+          <title>About PAge</title>
+          <meta name='description' content='Beginner friendly page for learning React Helmet.' />
+  </Helmet>
   <h1>This is About</h1>
   </>);
 }
