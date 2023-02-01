780b844d7273adc0978780ade08bbabf224fb527
alirazasdk <alirazasdk10@gmail.com>
2023-02-01 10:08:55 +0500

    Applied react-helemt-async inside home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import axios from "axios";
+import { Helmet } from 'react-helmet-async';
 const Home=()=>{
   useEffect(()=>{
     axios.get('https://jsonplaceholder.typicode.com/posts/1')
@@ -8,6 +9,10 @@ const Home=()=>{
     }))
   },[])
   return(<>
+  <Helmet>
+          <title>Learning React Helmet!</title>
+          <meta name='description' content='Beginner friendly page for learning React Helmet.' />
+        </Helmet>
   <h1>This is Home</h1>
   </>);
 }
